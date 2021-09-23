e0586482df6986a70b2073113ed2554e316b35ed
Hadi <hadilearn01@gmail.com>
2021-09-23 10:39:33 +0700

    create the database file

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 def _set_sqlite_pragma(dbapi_connection, connection_record):
     if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGME foreign_keys=ON;")
+        cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
 
 db = SQLAlchemy(app)
@@ -27,7 +27,15 @@ class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    email = db.Column(db.Sting(100))
+    email = db.Column(db.String(100))
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text)
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
